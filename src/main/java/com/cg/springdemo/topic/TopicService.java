e64a8b2b53f80ec60fdb313b0d4a9de0e19f71ae
rakrao <41562055+rakrao@users.noreply.github.com>
2018-08-20 17:49:41 +0530

    Add files via upload

---
+++ src/main/java/com/cg/springdemo/topic/TopicService.java
@@ -0,0 +1,42 @@
+package com.cg.springdemo.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	private List<Topic> topics = Arrays.asList(new Topic("Spring", "Spring Framework", "Spring description"),
+			new Topic("Java", "Java core", "Java description"),
+			new Topic("JavaScript", "JavaScript Framework", "JavaScript description"));
+
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		for (int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if (t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+
+	}
+
+	public void deleteTopic(String id) {
+
+		topics.remove(id);
+	}
+}
